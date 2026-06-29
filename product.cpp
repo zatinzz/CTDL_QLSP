c9f9cb8de192338d7b2f2b486b192185121d3b47
zatiinz <tiendhg4094@ut.edu.vn>
2026-06-29 17:37:37 +0700

    Update project

--- product.cpp
+++ product.cpp
@@ -1,1 +1,15 @@
-#include "product.h"
+#include "Product.h"
+
+Product::Product() {
+    id = 0;
+    name = "";
+    price = 0;
+    quantity = 0;
+}
+
+Product::Product(int id, string name, double price, int quantity) {
+    this->id = id;
+    this->name = name;
+    this->price = price;
+    this->quantity = quantity;
+}
--- product.h
+++ product.h
@@ -1,5 +1,18 @@
-#pragma once
-class product
-{
+#ifndef PRODUCT_H
+#define PRODUCT_H
+
+#include <string>
+
+using namespace std;
+
+struct Product {
+    int id;
+    string name;
+    double price;
+    int quantity;
+
+    Product();
+    Product(int id, string name, double price, int quantity);
 };
 
+#endif
