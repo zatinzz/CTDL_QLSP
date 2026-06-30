1f15797f38e98f6d0aebe101b7aba95760ab74ad
zatinzz <tientien4725@gmail.com>
2026-06-30 15:04:53 +0700

    Delete FileManager2.h

--- FileManager2.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "CategoryNode.h"
-#include <string>
-
-class FileManager {
-public:
-    static bool loadFromFile(const std::string& filename, CategoryTree& tree);
-    static bool saveToFile (const std::string& filename, const CategoryTree& tree);
-};
